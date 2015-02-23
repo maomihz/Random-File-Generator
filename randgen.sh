36a49200b9843dcba999aa2207db053cafa61948
U-maomi-PC\maomi <MaomiHz>
2015-02-23 12:27:36 +0800

    Initial Commit

---
+++ randgen.sh
@@ -0,0 +1,33 @@
+trap "clean" INT TERM QUIT KILL
+
+clean() {
+	cd ..
+	echo
+	echo cleaning...
+	if [ -d $CURFOLDER ]; then
+		rm -rf $CURFOLDER
+	fi
+	exit
+}
+
+gen() {
+	CURFOLDER=$(date '+%F_%H-%M-%S')
+	mkdir "$CURFOLDER"
+	cd "$CURFOLDER"
+	echo Generating Folder $CURFOLDER
+	for i in $(seq 1 100)
+	do
+		echo -n "generating file $i... "
+		FILENAME=$RANDOM
+		head -c $(($RANDOM*2048)) /dev/urandom | base64 >$FILENAME
+		echo $(du -h $FILENAME | cut -d '	' -f 1)
+		mv $FILENAME $(openssl sha1 $FILENAME | cut -d ' ' -f 2)
+	done
+	cd ..
+}
+
+echo "Program will not exit until you hit Ctrl-C"
+while true
+do
+	gen
+done
