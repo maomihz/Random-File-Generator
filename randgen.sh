d83b7fce5a2aa3f0428a222c74a3a792909beec7
U-maomi-PC\maomi <MaomiHz>
2015-02-24 09:35:39 +0800

    make genfolder() seperate function

--- randgen.sh
+++ randgen.sh
@@ -11,24 +11,30 @@ clean() {
 }
 
 gen() {
+	echo -n "generating file $i... "
+	FILENAME=$RANDOM.tmp
+	
+	cat /dev/urandom |
+	od -t x4 |
+	cut -d ' ' -f 2- |
+	tr -d ' ' |
+	tr -d '\n' |
+	fold -w 80 |
+	head -n $(($RANDOM * 32)) > $FILENAME
+	
+	echo $(du -h $FILENAME | cut -d '	' -f 1)
+	mv $FILENAME $(openssl sha1 $FILENAME | cut -d ' ' -f 2)
+
+}
+
+genfolder() {
 	CURFOLDER=$(date '+%F_%H-%M-%S')
 	mkdir "$CURFOLDER"
 	cd "$CURFOLDER"
 	echo Generating Folder $CURFOLDER
-	for i in $(seq 1 100)
-	do
-		echo -n "generating file $i... "
-		FILENAME=$RANDOM.tmp
-		cat /dev/urandom |
-		od -t x4 |
-		cut -d ' ' -f 2- |
-		tr -d ' ' |
-		tr -d '\n' |
-		fold -w 80 |
-		head -n $(($RANDOM * 32)) > $FILENAME
-		echo $(du -h $FILENAME | cut -d '	' -f 1)
-		mv $FILENAME $(openssl sha1 $FILENAME | cut -d ' ' -f 2)
-	done
+	for i in $(seq 1 100); do
+		gen
+	done 
 	cd ..
 }
 
@@ -44,5 +50,5 @@ rand() {
 echo "Program will not exit until you hit Ctrl-C"
 while true
 do
-	gen
+	genfolder
 done
