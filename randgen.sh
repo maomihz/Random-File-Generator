c6a7fb8137bd92009b78be459fbba292dc09e605
MaomiHz <maomihz@gmail.com>
2015-02-24 09:55:12 +0800

    generate base64 version as well

--- randgen.sh
+++ randgen.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 trap "clean" INT TERM QUIT KILL
 
 clean() {
@@ -10,15 +12,14 @@ clean() {
 	exit
 }
 
-gen() {
+genhex() {
 	echo -n "generating file $i... "
 	FILENAME=$RANDOM.tmp
 	
 	cat /dev/urandom |
 	od -t x4 |
 	cut -d ' ' -f 2- |
-	tr -d ' ' |
-	tr -d '\n' |
+	tr -d '[\ \n]' |
 	fold -w 80 |
 	head -n $(($RANDOM * 32)) > $FILENAME
 	
@@ -27,15 +28,12 @@ gen() {
 
 }
 
-genfolder() {
-	CURFOLDER=$(date '+%F_%H-%M-%S')
-	mkdir "$CURFOLDER"
-	cd "$CURFOLDER"
-	echo Generating Folder $CURFOLDER
-	for i in $(seq 1 100); do
-		gen
-	done 
-	cd ..
+gen() {
+	echo -n "generating file $i... "
+	FILENAME=$RANDOM.tmp
+	head -c $(($RANDOM*2048)) /dev/urandom | base64 >$FILENAME
+	echo $(du -h $FILENAME | cut -d '	' -f 1)
+	mv $FILENAME $(openssl sha1 $FILENAME | cut -d ' ' -f 2)
 }
 
 rand() {
@@ -47,8 +45,35 @@ rand() {
 }
 
 
-echo "Program will not exit until you hit Ctrl-C"
+case $1 in
+'-hex')
 while true
 do
-	genfolder
+	CURFOLDER=$(date '+%F_%H-%M-%S')
+	mkdir "$CURFOLDER"
+	cd "$CURFOLDER"
+	echo Generating Folder $CURFOLDER
+	for i in $(seq 1 100); do
+		genhex
+	done
+	cd ..
 done
+;;
+'-base64')
+while true
+do
+	CURFOLDER=$(date '+%F_%H-%M-%S')
+	mkdir "$CURFOLDER"
+	cd "$CURFOLDER"
+	echo Generating Folder $CURFOLDER
+	for i in $(seq 1 100); do
+		gen
+	done 
+	cd ..
+done
+;;
+*)
+	echo
+	echo "usage: $0 -hex|base64"
+;;
+esac
